923cd7188166bac0fe32aa15d4fa023809bc0991
Rojas Rafael <rafael.rojas@unibz.it>
2018-10-19 11:40:34 +0200

    update

--- test/pythonclient.py
+++ test/pythonclient.py
@@ -7,3 +7,6 @@ import xmlrpclib
 #socket.setdefaulttimeout(5)
 server = xmlrpclib.Server('http://10.10.238.33:5000')
 print(server.cdeletem("xmlrpc.test"))
+
+print('testing error')
+print(server.nonexistingmethod("xmlrpc.test"))
